1191b152c27d3ad5db6ea9d51eda18299d4cc6e0
Kyle Talbott <codekingkyle@gmail.com>
2021-03-11 13:39:30 -0500

    test fixes

--- test/leadMember.test.js
+++ test/leadMember.test.js
@@ -1,19 +1,19 @@
-const Manager = require("../lib/manager");
+const Manager = require("../lib/leadMember");
 
 test("can set office number via constructor", () => {
-  const testThis = "84";
-  const e = new Manager("Brian", 66, "test@test.com", testThis);
-  expect(e.officeNumber).toBe(testThis);
+    const testThis = "84";
+    const e = new Manager("kyle", 66, "test@test.com", testThis);
+    expect(e.phoneNumber()).toBe(testThis);
 });
 
 test("getRole() should return Manager", () => {
-  const testThis = "Manager";
-  const e = new Manager("Brian", 66, "test@test.com", 84);
-  expect(e.getRole()).toBe(testThis);
+    const testThis = "Manager";
+    const e = new Manager("kyle", 66, "test@test.com", 84);
+    expect(e.getRole()).toBe(testThis);
 });
 
 test("can get office number via getOfficeNumber()", () => {
-  const testThis = 84;
-  const e = new Manager("brian", 66, "test@test.com", testThis);
-  expect(e.getOfficeNumber()).toBe(testThis);
+    const testThis = 84;
+    const e = new Manager("kyle", 66, "test@test.com", testThis);
+    expect(e.getphonenumber()).toBe(testThis);
 });
--- test/teacher.test.js
+++ test/teacher.test.js
@@ -1,21 +1,20 @@
-
 const { TestScheduler } = require("jest");
-const Intern = require("../lib/intern");
+const Intern = require("../lib/Teacher");
 
 test("Can set school via constructor", () => {
-  const testThis = "Trilogy";
-  const e = new Intern("Jess", 9, "test@test", testThis);
-  expect(e.getSchool()).toEqual(testThis);
+    const testThis = "Trilogy";
+    const e = new Intern("Jess", 9, "test@test", testThis);
+    expect(e.getSchool()).toEqual(testThis);
 });
 
 test("can get school via getSchool()", () => {
-  const testThis = "Trilogy";
-  const e = new Intern("Jess", 9, "test@test.com", testThis);
-  expect(e.getSchool()).toBe(testThis);
+    const testThis = "Trilogy";
+    const e = new Intern("Jess", 9, "test@test.com", testThis);
+    expect(e.getSchool()).toBe(testThis);
 });
 
 test('getRole() should return "Intern"', () => {
-  const testThis = "Intern";
-  const e = new Intern("Jess", 9, "test@test.com", testThis);
-  expect(e.getRole()).toBe(testThis);
+    const testThis = "Teacher";
+    const e = new Intern("Jess", 9, "test@test.com", testThis);
+    expect(e.getRole()).toBe(testThis);
 });
