f4622c1132593bf3d53ed8e8f630779e7b06fc8a
Mr. Spencer <codekingkyle@gmail.com>
2021-03-12 16:06:44 -0500

    fixes

--- test/leadMember.test.js
+++ test/leadMember.test.js
@@ -3,17 +3,17 @@ const Manager = require("../lib/leadMember");
 test("can set office number via constructor", () => {
     const testThis = "84";
     const e = new Manager("kyle", 66, "test@test.com", testThis);
-    expect(e.phoneNumber()).toBe(testThis);
+    expect(e.getyearsWorked()).toBe(testThis);
 });
 
-test("getRole() should return Manager", () => {
-    const testThis = "Manager";
+test("getRole() should return leadMember", () => {
+    const testThis = "leadMember";
     const e = new Manager("kyle", 66, "test@test.com", 84);
     expect(e.getRole()).toBe(testThis);
 });
 
 test("can get office number via getOfficeNumber()", () => {
     const testThis = 84;
-    const e = new Manager("kyle", 66, "test@test.com", testThis);
-    expect(e.getphonenumber()).toBe(testThis);
+    const e = new Manager("kyle", testThis, "test@test.com", 14);
+    expect(e.getphoneNumber()).toBe(testThis);
 });
